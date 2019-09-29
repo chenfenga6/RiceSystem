4a68da5f6bccf2c017bd2285d86e40063b950ece
28023 <2802329365@qq.com>
2019-09-29 11:16:39 +0800

    修改了platform中的两个文件

--- src/main/java/com/test/Service/lmpl/PlatformServicelmpl.java
+++ src/main/java/com/test/Service/lmpl/PlatformServicelmpl.java
@@ -1,19 +1,15 @@
 package com.test.Service.lmpl;
 
 import com.test.Dao.PlatformDao;
-import com.test.Dao.TreeDao;
 import com.test.Entity.Platform;
 import com.test.Service.PlatformService;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class PlatformServicelmpl implements PlatformService {
-    @Resource
-    TreeDao treeDao;
     @Resource
     PlatformDao platformDao;
 
@@ -22,49 +18,61 @@ public class PlatformServicelmpl implements PlatformService {
         //第一步，在platform_data表中插入数据
         int ret = platformDao.insertPlatform(platform.getPname(), platform.getPcode(),
                 platform.getPurl(), platform.getPlog());
-        if(ret < 0)
+        if(ret <= 0)
             return "add_faile";
 
-        Integer Pid = platformDao.getLastPid();
-        //第二步，新建一张表，表名为platform+pid
+        //第二步，新建一张表，表名: platform+pid
+        Integer Pid = platformDao.getLatestPid();
         System.out.println("表名：platform"+Pid);
-        String table ="platform"+Pid;
-        ret = platformDao.createTab(table);
+
+        String tableName ="platform"+Pid;
+        ret = platformDao.createTab(tableName);
         if(ret != 0)
             return "create_fail";
 
         //第三步,在新建表中插入根节点
-        ret = treeDao.addTreeNode(table,platform.getPname(),null,0,null,null);
-        if(ret == 1){
+        ret = platformDao.addTreeNode(tableName, platform.getPname(),null,0,null,null);
+        if(ret == 1) {
             return "success";
         }
         return "insert_fail";
     }
 
-    //删除平台，删除platform_data表的一条记录，删除平台的那张表
+    //删除平台
     public String platform_delete(Integer pid) {
+        //第一步，删除平台（platform_data表的一条记录）
         int ret = platformDao.deletePlatform(pid);
         if(ret < 0)
             return "fail";
+
+        //第二步，删除平台的那张表
         ret = platformDao.deleteTab("platform" + pid.toString());
-        if(ret == 0)
+        if(ret < 0)
+            return "fail";
+
+        //第三步，删除permission表里的相关数据
+        ret = platformDao.deletePermByPid(pid);
+        System.out.println(ret);
+        if(ret >= 0)
             return "success";
         return "fail";
     }
 
     //修改平台信息
     public String platform_change(Platform platform) {
-        Platform p = new Platform();
-        p = platformDao.findByPid(platform.getPid());
-        if(!( ( platform.getPname() ).equals( p.getPname() ) ) ){ //判断 是否 更改平台名属性
-            treeDao.changeTreeNodeCname("platform"+platform.getPid(),platform.getPname(),1);  //修改该平台对应的根结点名字
+        //如果修改平台名，需要修改平台表里对应的根结点名字
+        Platform oldPlat = platformDao.findByPid(platform.getPid());
+        if(!((platform.getPname()).equals(oldPlat.getPname()))){
+            String tableName = "platform" + platform.getPid();
+            platformDao.updateNodeName(tableName, platform.getPname(), 1);
         }
 
-        int ret = platformDao.updatePlatform(platform.getPid(), platform.getPname(),        //修改平台信息
+        //修改平台信息
+        int ret = platformDao.updatePlatform(platform.getPid(), platform.getPname(),
                 platform.getPcode(), platform.getPurl(), platform.getPlog());
-
-        if(ret == 1)
+        if(ret == 1) {
             return "success";
+        }
         return "fail";
     }
 
